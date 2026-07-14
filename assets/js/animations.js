1d6e102c4f76dd84469d52f12097787b5f9268ba
ShohidulClaw <shohidulclaw@gmail.com>
2026-07-14 22:15:54 +0600

    Create animations.js

---
+++ assets/js/animations.js
@@ -0,0 +1,19 @@
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:.15
+});
+
+document
+.querySelectorAll(".animate")
+.forEach(item=>observer.observe(item));
